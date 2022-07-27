6bbabbba064f79bc0c62e06e005c48c63e7d97b3
Aris Karagiannidis <arkaragian@gmail.com>
2022-07-28 01:37:46 +0300

    Add initial dap settings

--- init.lua
+++ init.lua
@@ -4,6 +4,9 @@ vim.call('plug#begin')
 Plug 'https://github.com/vim-latex/vim-latex.git'
 Plug 'https://github.com/neovim/nvim-lspconfig'
 Plug 'tomasiser/vim-code-dark'
+--Plugins for debugging
+Plug 'mfussenegger/nvim-dap'
+Plug 'rcarriga/nvim-dap-ui'
 vim.call('plug#end')
 
 
@@ -111,3 +114,36 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+
+local dap = require('dap')
+dap.set_log_level('INFO')
+dap.adapters.csharp = {
+  type = 'executable',
+  command = 'C:\\Users\\Aris\\bin\\netcoredbg\\netcoredbg.exe',
+  args = {'--interpreter=vscode',
+	  '--engineLogging=C:\\Users\\Aris\\enginelog.log',
+	  '--log=C:\\Users\\Aris\\log.log',
+	 },
+}
+
+dap.configurations.cs = {
+  {
+    type = "csharp",
+    name = "launch - netcoredbg",
+    request = "launch",
+    --TODO: make the /bin/Debug configurable
+    program = function()
+        return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/net6.0/test.dll', 'file')
+    end,
+  },
+}
+
+
+vim.keymap.set('n', '<space>b', ":lua require'dap'.toggle_breakpoint()<CR>", opts) --Toggle Breakpoint
+vim.keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>", opts) --Toggle Breakpoint
+vim.keymap.set('n', '<F10>', ":lua require'dap'.step_over()<CR>", opts) --Toggle Breakpoint
+vim.keymap.set('n', '<F11>', ":lua require'dap'.step_into()<CR>", opts) --Toggle Breakpoint
+vim.keymap.set('n', '<F12>', ":lua require'dap'.step_out()<CR>", opts) --Toggle Breakpoint
+
+require("dapui").setup()
