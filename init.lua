e97ed45fa134e24439527035a7db5d0a53e61a36
Aris Karagiannidis <arkaragian@gmail.com>
2022-07-28 23:58:56 +0300

    Add lspkind and cmp-buffer plugins

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 --Call the vim-plug using the vimscript methods since vim plug does not have native lua calls.
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
-Plug 'https://github.com/vim-latex/vim-latex.git'
+--Plug 'https://github.com/vim-latex/vim-latex.git'
 Plug 'https://github.com/neovim/nvim-lspconfig'
 Plug 'tomasiser/vim-code-dark'
 --Plugins for debugging
@@ -10,8 +10,10 @@ Plug 'rcarriga/nvim-dap-ui'
 
 --Plugins for code completion. We use the cmp plugin. This also requires completion
 --sources. For now we only use the lsp source
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp' -- Source for internal nvim lua completion
+Plug 'hrsh7th/cmp-buffer' -- Cmp source buffer
+Plug 'onsails/lspkind.nvim' -- Formating of completion sources
+Plug 'hrsh7th/nvim-cmp' -- Autocompletion engine
 vim.call('plug#end')
 
 require('behavior')
