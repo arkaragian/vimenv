ea873a50f534c313ceee9384320729d794d75cbb
Aris Karagiannidis <arkaragian@gmail.com>
2022-08-29 00:10:24 +0300

    Add initial implementation for the formating logic

--- lua/Formatting.lua
+++ lua/Formatting.lua
@@ -1,2 +1,21 @@
--- Define bindings for invoking external programs when formatting command has been called.
--- This is WIP.
+-- Define bindings for invoking external programs when formatting command has been called. This is still WIP.
+function FormatFile()
+  local filetype = vim.bo.filetype
+  print("Filetype is " .. filetype)
+  -- Implement here based on our filetype what command we should use to make this work.
+end
+
+-- Setup any keybindings we want for formating our file.
+function SetupKeyBindings()
+ vim.keymap.set('n','<leader>f',FormatFile)
+end
+
+-- Register format
+local FormatAutoGroup = vim.api.nvim_create_augroup("FormatAutoGroup", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = {"*.h","*.c","*.cpp"},
+  callback = SetupKeyBindings, 
+  group = FormatAutoGroup,
+})
+
--- lua/languageServer.lua
+++ lua/languageServer.lua
@@ -22,7 +22,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+  --vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
 
   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, bufopts)
