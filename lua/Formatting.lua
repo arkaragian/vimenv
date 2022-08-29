51bcb23379b925487bab315d92e0176bb17c2854
Aris Karagiannidis <akar@miltech.gr>
2022-08-29 10:18:31 +0300

    Implement basic formating logic

--- lua/Formatting.lua
+++ lua/Formatting.lua
@@ -1,8 +1,18 @@
 -- Define bindings for invoking external programs when formatting command has been called. This is still WIP.
+
+--- Format a file using the appropriate third party tool. Assume that this tool is in our path.
 function FormatFile()
   local filetype = vim.bo.filetype
   print("Filetype is " .. filetype)
-  -- Implement here based on our filetype what command we should use to make this work.
+  if filetype == "c" or filetype == "cpp" or filetype == ".h" then
+    print("Formatiing using clang " .. vim.api.nvim_buf_get_name(0))
+
+    -- I/O Pipe open execute clang. -i argument means format the file in place.
+    io.popen("clang-format -i " .. vim.api.nvim_buf_get_name(0))
+    return
+  end
+
+  print("No formating rule found. Doing nothing")
 end
 
 -- Setup any keybindings we want for formating our file.
