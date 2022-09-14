499c90e96f1e9f4d6db4083d715c4e1680facb2f
Aris Karagiannidis <arkaragian@gmail.com>
2022-09-15 00:40:10 +0300

    Enable nvim internal api completion

--- lua/LanguageServer.lua
+++ lua/LanguageServer.lua
@@ -86,15 +86,43 @@ require('lspconfig').clangd.setup {
 
 -- To get builtin LSP running, do something like:
 -- NOTE: This replaces the calls where you would have before done `require('nvim_lsp').sumneko_lua.setup()`
-require('nlua.lsp.nvim').setup(require('lspconfig'), {
-  on_attach = on_attach,
+--require('nlua.lsp.nvim').setup(require('lspconfig'), {
+--  on_attach = on_attach,
 
   -- Include globals you want to tell the LSP are real :)
   --globals = {
     -- Colorbuddy
   --  "Color", "c", "Group", "g", "s",
   --}
-})
+--})
+-- Example custom configuration for lua
+--
+-- Make runtime files discoverable to the server
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
+require('lspconfig').sumneko_lua.setup {
+  cmd = {"C:/Users/Aris/bin/lua-language-server-3.5.5-win32-x64/bin/lua-language-server.exe"},
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = { enable = false, },
+    },
+  },
+}
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 --
