9a0ddde5c2fea889fbba36da562767b44f31497f
Aris Karagiannidis <akar@miltech.gr>
2022-11-06 21:25:26 +0200

    refactor: update behavior

--- lua/Behavior.lua
+++ lua/Behavior.lua
@@ -13,6 +13,7 @@ require('vscode').setup({
     italic_comments = true,
 })
 
+
 -- Display invisible characters
 vim.cmd('set invlist')
 
@@ -50,15 +51,19 @@ opt.autoread = true
 vim.g.mapleader = ' '
 
 --Add two characters when typing the tab key
-opt.tabstop = 2
-opt.softtabstop = 2
+opt.tabstop = 4         -- Number of spaces that a <Tab> in the file counts for.
+opt.softtabstop = 4     -- Number of spaces that a <Tab> counts for while performing editing operations, like inserting a <Tab> or using <BS>.
+
+
 -- Control the identation side
-opt.shiftwidth = 2
+opt.shiftwidth = 4      -- Number of characters that will be used for identation
 opt.listchars = {
-	tab ='->',
+    tab ='<->',         -- This will make the tab appear as > <> or <-- (n-times for dash) -->
 }
--- Do not use spaces instead of tabs when pressing the tab key
-opt.expandtab = false
+
+-- Use spaces instead of tabs when pressing the tab key. To insert a tab literally
+-- Type CTRL+q (Swith to literal mode for the nex char) then 
+opt.expandtab = true
 
 --Set file ending to unix. This is important for bash scripts
 opt.ff = "unix"
@@ -70,3 +75,7 @@ vim.keymap.set('n','bh',vim.cmd.bprevious)
 
 -- Configure our shell to be able to execute the commands from within VIM
 opt.shell='cmd.exe'
+
+
+-- Configure CTRL+C in the clipboard register
+vim.keymap.set('v','<C-c>','"+y')
