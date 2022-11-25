7187c80b22b35d3ad85eea54d89f0d765ed30c0e
Aris Karagiannidis <akar@miltech.gr>
2022-11-26 01:29:27 +0200

    feat: enable debug adapter for C/C++/Rust

--- lua/DebugAdapter.lua
+++ lua/DebugAdapter.lua
@@ -7,6 +7,13 @@ dap.adapters.csharp = {
   args = {'--interpreter=vscode', '--engineLogging='..os.getenv("HOME")..'/NetCoreDebugEnginelog.log'},
 }
 
+
+dap.adapters.lldb = {
+  type = 'executable',
+  command = 'lldb-vscode', -- lldb-vscode is in our path
+  name = 'lldb'
+}
+
 dap.configurations.cs = {
   {
     type = "csharp",
@@ -19,12 +26,42 @@ dap.configurations.cs = {
   },
 }
 
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/a.exe', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+
+    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+    --
+    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    --
+    -- Otherwise you might get the following error:
+    --
+    --    Error on launch: Failed to attach to the target process
+    --
+    -- But you should be aware of the implications:
+    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+    -- runInTerminal = false,
+  },
+}
+
+-- Use the same configuration for C and rust
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
 
 vim.keymap.set('n', '<space>b', ":lua require'dap'.toggle_breakpoint()<CR>", opts) --Toggle Breakpoint
 vim.keymap.set('n', '<F5>', ":lua require'dap'.continue()<CR>", opts) --Toggle Breakpoint
 vim.keymap.set('n', '<F10>', ":lua require'dap'.step_over()<CR>", opts) --Toggle Breakpoint
 vim.keymap.set('n', '<F11>', ":lua require'dap'.step_into()<CR>", opts) --Toggle Breakpoint
 vim.keymap.set('n', '<F12>', ":lua require'dap'.step_out()<CR>", opts) --Toggle Breakpoint
+vim.keymap.set('n', '<F1>', ":lua require'dap'.terminate()<CR>", opts) --Toggle Breakpoint
 vim.keymap.set('n', 'os', ":lua require'dapui'.open()<CR>", opts) --Toggle Breakpoint
 vim.keymap.set('n', 'cs', ":lua require'dapui'.close()<CR>", opts) --Toggle Breakpoint
 
