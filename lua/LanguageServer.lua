25a2128ebb34f0af05174efd8a52c97d92019a12
Aris Karagiannidis <akar@miltech.gr>
2022-12-06 17:51:58 +0200

    add comments and change keybindings

--- lua/LanguageServer.lua
+++ lua/LanguageServer.lua
@@ -14,14 +14,14 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts) -- Shorthand for "workspace add"
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)-- Shorthand for "workspace remove"
   vim.keymap.set('n', '<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
+  end, bufopts) --Shorthand for "workspace list"
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts) -- Shorthand for "Code Actions"
+  vim.keymap.set('n', '<leader>fr', vim.lsp.buf.references, bufopts)  -- Shorthand for "Find references"
   --vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
 
   vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
@@ -30,7 +30,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, bufopts)
   
   -- Add some common functionallity here
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts) -- Shorthand for "Rename". This would rename the symbol under the cursor
 end
 
 local omnisharp_bin = "OmniSharp.exe"
