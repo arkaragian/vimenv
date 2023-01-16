3b240bb9e8ccc61bb6c1e28fcd6241808d3ca4be
Aris Karagiannidis <akar@miltech.gr>
2023-01-16 09:40:38 +0200

    feat: apply playground configuration
    Apply formatting

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ Plug 'hrsh7th/nvim-cmp' -- Autocompletion engine
 
 -- Tree Sitter
 Plug ('nvim-treesitter/nvim-treesitter',{['do'] = vim.fn[':TSUpdate'], tag = 'v0.8.1'})
+Plug 'nvim-treesitter/playground'
 
 -- Telescope dependencies and telescope
 Plug ('nvim-lua/plenary.nvim', {tag='v0.1.2'})
