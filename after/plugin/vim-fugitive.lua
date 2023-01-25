a4348e72baf51386dbe12b953bd4d16aa19887fd
Aris Karagiannidis <akar@miltech.gr>
2023-01-25 12:32:50 +0200

    feat: add vim-figitive keymaps

---
+++ after/plugin/vim-fugitive.lua
@@ -0,0 +1,3 @@
+
+vim.keymap.set('n', '<leader>gs', ":Git status<CR>", {desc = "Git Status"}) -- Find in all files
+vim.keymap.set('n', '<leader>gd', ":Gdiffsplit<CR>", {desc = "Git diff"}) -- Find in all files
