64952bc08b64381a386b366aa7d712a15fd90862
Aris Karagiannidis <akar@miltech.gr>
2023-01-25 16:38:03 +0200

    refactor: add git commit mapping

--- after/plugin/vim-fugitive.lua
+++ after/plugin/vim-fugitive.lua
@@ -1,3 +1,4 @@
 
-vim.keymap.set('n', '<leader>gs', ":Git status<CR>", {desc = "Git Status"}) -- Find in all files
-vim.keymap.set('n', '<leader>gd', ":Gdiffsplit<CR>", {desc = "Git diff"}) -- Find in all files
+vim.keymap.set('n' , '<leader>gs' , ":Git status<CR>" , {desc = "Git Status"}) -- Find in all files
+vim.keymap.set('n' , '<leader>gd' , ":Gdiffsplit<CR>" , {desc = "Git diff"}) -- Find in all files
+vim.keymap.set('n' , '<leader>gc' , ":Gcommit<CR>"    , {desc = "Git Commit"}) -- Find in all files
