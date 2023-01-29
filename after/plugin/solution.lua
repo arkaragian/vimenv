c3c34e8852c62e44fd01883f0510f1c436fe7510
Aris Karagiannidis <akar@miltech.gr>
2023-01-30 01:04:44 +0200

    chore: plugin testing changes

--- after/plugin/solution.lua
+++ after/plugin/solution.lua
@@ -1,9 +1,8 @@
 local sol = require("solution")
 
 SolutionConfig = {
-    selection = "first",
-    ext = ".csproj",
-    conf = "Debug",
+    ProjectSelectionPolicy = "first",
+    BuildConfiguration = "Debug",
     arch = "x64",
     display = {
         removeCR = true
@@ -15,3 +14,5 @@ sol.setup(SolutionConfig)
 vim.keymap.set('n' , '<leader>cc' , sol.Compile , {desc="CS Solution Compile"})
 vim.keymap.set('n' , '<leader>cl' , sol.Clean   , {desc="CS Solution clean"})
 vim.keymap.set('n' , '<leader>st' , sol.Test   , {desc="Test a Plugin Function"})
+vim.keymap.set('n' , '<leader>ft' , sol.GetTests   , {desc="Test a Plugin Function"})
+vim.keymap.set('n' , '<leader>sss' , sol.FunctionTest   , {desc="Test a Plugin Function"})
