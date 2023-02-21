ac551e69c1361230fd441401e2503974cc79e202
Aris Karagiannidis <akar@miltech.gr>
2023-02-22 00:25:09 +0200

    feat: update treesitter objects

--- after/plugin/nvim-treesitter-textobjects.lua
+++ after/plugin/nvim-treesitter-textobjects.lua
@@ -14,6 +14,12 @@ require("nvim-treesitter.configs").setup {
         -- You can optionally set descriptions to the mappings (used in the desc parameter of
         -- nvim_buf_set_keymap) which plugins like which-key display
         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+
+        ["aa"] = {query = "@parameter.outer", desc = "Select outer parameter"},
+        ["ia"] = {query = "@parameter.inner", desc = "Select inner parameter"},
+
+        ["av"] = "@variable.outer",
+        ["iv"] = "@variable.inner",
       },
       -- You can choose the select mode (default is charwise 'v')
       --
@@ -51,7 +57,7 @@ require("nvim-treesitter.configs").setup {
         --
         -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
         -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-        ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+        -- ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" }, <-- This interferes with spell check
         ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
       },
       goto_next_end = {
