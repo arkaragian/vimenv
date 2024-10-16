0f6fda29353cad7ccfc2f2d5d657a7f6bcef0027
Aris Karagiannidis <akar@miltech.gr>
2024-10-16 08:07:05 +0300

    chore: experiment with custom kanawaga theme

--- lua/plugins/kanawaga.lua
+++ lua/plugins/kanawaga.lua
@@ -20,8 +20,44 @@ return {
         commit = 'f491b0fe68fffbece7030181073dfe51f45cda81',
         lazy = false,
         priority = 1000,
-        -- config = function()
-        --     vim.cmd.colorscheme("kanagawa")
-        -- end
-    },
+        opts = {
+            overrides = function(colors)
+                return {
+                    -- Override LaTeX-specific highlights
+                    --["@text.environment"] = { fg = "#957FB8" }, -- Set the foreground color for LaTeX environments
+                    ["@text.environment"] = { fg = "#FFFFFF" }, -- Set the foreground color for LaTeX environments
+                    -- Add more overrides as needed
+                }
+            end,
+        },
+        -- opts = {
+        --     overrides = {
+        --     -- create a new hl-group using default palette colors and/or new ones
+        --     -- This matches the names returned from :TSHighlightCapturesUnderCursor
+        --     -- The colors are selected after looking alot in the kanagawa repository.
+        --     {
+        --         ["@text.environment"] = { fg = "#957FB8" },
+        --         -- ["@text.environment.name"] = { fg = default_colors.st },
+        --     }
+        --     }
+        -- }
+        -- -- config = function()
+        --     --     vim.cmd.colorscheme("kanagawa")
+        --     -- end
+        -- },
+        -- opts = {
+        --     overrides = function(colors)
+        --         return {
+        --             -- Override LaTeX-specific highlights
+        --             ["@text.environment"] = { fg = "#957FB8" }, -- Set the foreground color for LaTeX environments
+        --             -- Add more overrides as needed
+        --         }
+        --     end,
+        -- },
+        -- config = function(_, opts)
+        --     require('kanagawa').setup(opts)
+        --     -- Apply the colorscheme
+        --     vim.cmd('colorscheme kanagawa')
+        -- end,
+    }
 }
