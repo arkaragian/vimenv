5e698cdd5a69ba5cab78c3ec01ef20002ec9ec8a
Aris Karagiannidis <akar@miltech.gr>
2026-02-13 22:28:09 +0200

    feat: reimplement textobjects with latest text objects

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,40 +5,87 @@ return {
         build = ":TSUpdate",
         branch = "main",
         config = function()
-            require("nvim-treesitter").setup({
-
-                indent = { enable = true },
-                folds = { enable = true },
-
-                highlight = {
-                    enable = true,
-                    -- Disable the highlighting here in order
-                    -- to let rainbow csv to take over.
-                    disable = { "csv" }
-                },
-
-                ensure_installed = {
-                    "vim",
-                    "c",
-                    "lua",
-                    "rust",
-                    "c_sharp",
-                    "razor",
-                    "latex",
-                    "cmake",
-                    "cpp",
-                    "json",
-                    "make",
-                    "python",
-                    "sql",
-                    "html",
-                    "javascript",
-                    --"nu",
-                    "query" -- query parser for query editor highlighting
-                }
-
-
-            })
+            require("nvim-treesitter").setup({})
         end
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+        init = function()
+            -- Disable entire built-in ftplugin mappings to avoid conflicts.
+            -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+            vim.g.no_plugin_maps = true
+
+            -- Or, disable per filetype (add as you like)
+            -- vim.g.no_python_maps = true
+            -- vim.g.no_ruby_maps = true
+            -- vim.g.no_rust_maps = true
+            -- vim.g.no_go_maps = true
+        end,
+        config = function()
+            -- -- configuration
+            require("nvim-treesitter-textobjects").setup {
+                select = {
+                    -- Automatically jump forward to textobj, similar to targets.vim
+                    lookahead = true,
+                    -- You can choose the select mode (default is charwise 'v')
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * method: eg 'v' or 'o'
+                    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                    -- mapping query_strings to modes.
+                    selection_modes = {
+                        ['@parameter.outer'] = 'v', -- charwise
+                        ['@function.outer'] = 'V', -- linewise
+                        ['@class.outer'] = '<c-v>', -- blockwise
+                    },
+                    -- If you set this to `true` (default is `false`) then any textobject is
+                    -- extended to include preceding or succeeding whitespace. Succeeding
+                    -- whitespace has priority in order to act similarly to eg the built-in
+                    -- `ap`.
+                    --
+                    -- Can also be a function which gets passed a table with the keys
+                    -- * query_string: eg '@function.inner'
+                    -- * selection_mode: eg 'v'
+                    -- and should return true of false
+                    include_surrounding_whitespace = false,
+                },
+            }
+
+            -- keymaps
+            -- You can use the capture groups defined in `textobjects.scm`
+            vim.keymap.set({ "x", "o" }, "am", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "im", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ac", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ic", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+            end)
+
+            vim.keymap.set({ "x", "o" }, "ip", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@parameter.inner", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ap", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@parameter.outer", "textobjects")
+            end)
+
+            -- For Latex
+            vim.keymap.set({ "x", "o" }, "ie", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@block.inner", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ae", function()
+                require "nvim-treesitter-textobjects.select".select_textobject("@block.outer", "textobjects")
+            end)
+        end,
     }
 }
